6c8bc0295b01047c347080c5330fc9d88e93ddc7
anievas <arturo98nievas@gmail.com>
2024-12-04 19:59:43 -0300

    Merge Facultades

---
+++ src/core/models/usuario/ususario.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from src.core.database import db
+
+class Usuario(db.Model):
+    __tablename__ = 'usuario'
+
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    apellido = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    contraseña = db.Column(db.String(100), nullable=False)
+    id_rol = db.Column(db.Integer, db.ForeignKey('rol.id'), nullable=True)
+    rol = db.relationship('Rol', back_populates='usuarios')
+    creacion = db.Column(db.DateTime, default=datetime.now)
+    actualizacion = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+
+    def __repr__(self):
+        return f"<Usuario {self.id} {self.nombre} {self.apellido} {self.email} {self.rol.nombre}>"
