c823948f24c27c55da8e9f0c2c3ac90db37b57ad
JOAQUíN MANUEL GONZALEZ <joaquinmgonzalez16@outlook.com>
2024-12-05 12:47:53 -0300

    avances en el portal para alumnos

--- src/web/controllers/alumnos_controller.py
+++ src/web/controllers/alumnos_controller.py
@@ -3,3 +3,8 @@ from src.core.services import alumno_service
 
 
 alumnos_bp = Blueprint("alumnos_bp", __name__, url_prefix="/alumnos")
+
+
+@alumnos_bp.get("/")
+def index():
+    return render_template("alumnos/index.html")
