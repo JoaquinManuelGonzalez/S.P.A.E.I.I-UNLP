9a209a607020c0b88dd691d2412ccd51a3f5fc01
JOAQUíN MANUEL GONZALEZ <joaquinmgonzalez16@outlook.com>
2024-12-06 16:09:38 -0300

    listar para alumnos, inicio

--- src/web/controllers/alumnos_controller.py
+++ src/web/controllers/alumnos_controller.py
@@ -9,3 +9,27 @@ alumnos_bp = Blueprint("alumnos_bp", __name__, url_prefix="/alumnos")
 @alumnos_bp.get("/")
 def index():
     return render_template("alumnos/index.html")
+
+
+@alumnos_bp.get("/listar-alumnos")
+def listar_alumnos():
+
+    nombre = request.args.get("nombre")
+    apellido = request.args.get("apellido")
+    email = request.args.get("email")
+    pagina = request.args.get("pagina", 1, type=int)
+    ordenado_por = request.args.get("ordenado_por", "nombre")
+    orden = request.args.get("orden", "asc")
+    por_pagina = 5
+
+    alumnos = alumno_service.filtar_alumnos(
+        nombre,
+        apellido,
+        email,
+        pagina,
+        ordenado_por,
+        orden,
+        por_pagina
+    )
+
+    return render_template("alumnos/listar-alumnos.html", alumnos=alumnos)
