9e46f15f72cf8e25c9e8df0c88c866418a704ab0
JOAQUíN MANUEL GONZALEZ <joaquinmgonzalez16@outlook.com>
2024-12-06 12:13:41 -0300

    primer envio de mail

--- src/web/__init__.py
+++ src/web/__init__.py
@@ -7,6 +7,7 @@ from flask_wtf.csrf import CSRFProtect
 from src.web.seeds import seed_countries, seed_generos, seed_estados_civiles, seeds_usuarios
 from src.web.controllers.routes import registrar_rutas
 from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity, jwt_required
+from flask_mail import Mail, Message
 
 
 #session = Session()
@@ -27,10 +28,22 @@ def create_app(env="development", static_folder="../../static", template_folders
     csrf = CSRFProtect(app)
     app = registrar_rutas(app)
     jwt = JWTManager(app)
+    mail = Mail(app)
     
     #session.init_app(app)
 
+    @app.route("/send_email")
+    def send_email():
+        try:
+            # Crear el mensaje
+            msg = Message("Prueba de correo desde Flask", recipients=["joaquinmgonzalez16@outlook.com"])
+            msg.body = "Este es un correo de prueba desde Flask utilizando Gmail."
 
+            # Enviar el correo
+            mail.send(msg)
+            return "Correo enviado con éxito"
+        except Exception as e:
+            return f"Hubo un error al enviar el correo: {str(e)}"
 
     @app.route("/")
     def home():
