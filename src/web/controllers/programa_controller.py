e0e12a226629e5c32124fb0eb8c3f3224325ed64
Lautaro Gutierrez <lautygutierrez5@gmail.com>
2025-01-23 21:19:42 -0300

    eliminar y editar programa

--- src/web/controllers/programa_controller.py
+++ src/web/controllers/programa_controller.py
@@ -28,4 +28,28 @@ def listar_programas():
     programas = programa_service.listar_programas()
     return render_template('programas/listar_programas.html', programas=programas)
 
+@bp.get('/editar_programa/<int:id>')
+def editar_programa(id):
+    programa = programa_service.get_programa_by_id(id)
+    form = ProgramaForm(obj=programa)
+    return render_template('programas/editar_programa.html', form=form, id=id)
+
+@bp.post('/actualizar_programa/<int:id>')
+def actualizar_programa(id):
+    form = ProgramaForm()
+    if form.validate_on_submit():
+        nombre = form.nombre.data
+        if not nombre:
+            flash('El nombre es requerido', 'danger')
+            return redirect(url_for('programa.editar_programa', id=id))
+        programa_service.actualizar_programa(id, nombre)
+        return redirect(url_for('programa.listar_programas'))
+    flash('Error al actualizar el programa', 'danger')
+    return redirect(url_for('programa.editar_programa', id=id))
+
+@bp.post('/eliminar_programa/<int:id>')
+def eliminar_programa(id):
+    programa_service.eliminar_programa(id)
+    return redirect(url_for('programa.listar_programas'))
+
     
