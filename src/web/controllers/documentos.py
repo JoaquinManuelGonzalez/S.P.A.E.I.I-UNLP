1504c2eafb941d3254dcb18865bfa63ad33a1eee
anievas <arturo98nievas@gmail.com>
2025-02-03 17:02:23 -0300

    Agregados templates de carta aceptacion y de certificado de calificaciones, junto con controlladores para descargar el contenido

---
+++ src/web/controllers/documentos.py
@@ -0,0 +1,86 @@
+from datetime import datetime
+from flask import Blueprint, flash, redirect, request, render_template, url_for, send_file
+from src.core.services import asignaturas as asignaturas_service
+from src.core.services import postulacion_service
+from src.web.handlers.permisos import check
+from weasyprint import HTML
+import io
+
+
+documentos_bp = Blueprint("documentos", __name__, url_prefix="/documentos")
+
+#-----Descargar certificad calificaciones-----
+@documentos_bp.get('/certificado_calificaciones/<int:postulacion_id>')
+
+def certificado_calificaciones(postulacion_id):
+
+    previous_url = request.referrer
+
+    postulacion = postulacion_service.get_postulacion_by_id(postulacion_id)
+
+    if postulacion is None:
+        flash("No existe la postulación.", "error")
+        return redirect(previous_url)
+    
+    asignaturas = postulacion.asignaturas
+
+    if asignaturas is None or asignaturas.count() == 0:
+        flash("No se han elegido materias para esta postulación.", "error")
+        return redirect(previous_url)
+    
+    notas_cerradas = True
+    for a in asignaturas:
+        if a.calificacion < 0:
+            notas_cerradas = False
+    
+    if not notas_cerradas:
+        flash("No se han cerrado las notas de todas las materias.", "error")
+        return redirect(previous_url)
+
+    html_string = render_template("certificado.html", postulacion=postulacion, fecha_firma=datetime.now())
+    pdf_bytes = HTML(string=html_string).write_pdf()
+
+    return send_file(
+        io.BytesIO(pdf_bytes),
+        mimetype="application/pdf",
+        as_attachment=True,
+        download_name="certificado_calificaciones_" + postulacion.informacion_alumno_entrante.apellido + ".pdf"
+    )
+
+#-----Descargar carta de aceptacion-----
+@documentos_bp.get('/carta_aceptacion/<int:postulacion_id>')
+
+def carta_aceptacion(postulacion_id):
+
+    previous_url = request.referrer
+
+    postulacion = postulacion_service.get_postulacion_by_id(postulacion_id)
+
+    if postulacion is None:
+        flash("No existe la postulación.", "error")
+        return redirect(previous_url)
+    
+    asignaturas = postulacion.asignaturas
+
+    if asignaturas is None or asignaturas.count() == 0:
+        flash("No se han elegido materias para esta postulación.", "error")
+        return redirect(previous_url)
+    
+    materias_validadas = True
+    for a in asignaturas:
+        if not a.validada:
+            materias_validadas = False
+    
+    if not materias_validadas:
+        flash("No se han validado todas las materias seleccionadas.", "error")
+        return redirect(previous_url)
+
+    html_string = render_template("certificado.html", postulacion=postulacion, fecha_firma=datetime.now())
+    pdf_bytes = HTML(string=html_string).write_pdf()
+
+    return send_file(
+        io.BytesIO(pdf_bytes),
+        mimetype="application/pdf",
+        as_attachment=True,
+        download_name="certificado_calificaciones_" + postulacion.informacion_alumno_entrante.apellido + ".pdf"
+    )
